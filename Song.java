fed6d3f14989f0ffee423cc865564b63cd478303
Jazmyn Doughty <>
2021-04-07 18:36:52 +0000

    Initial commit

---
+++ Song.java
@@ -0,0 +1,26 @@
+public class Song {
+
+  private Artist artist;
+  private String name;
+	public Song(Artist _artist, String _name) {
+    artist = _artist;
+    name = _name;
+	}
+
+	public String getName() {
+	  return name;
+  }
+
+	public Artist getArtist() {
+    return artist;
+	}
+
+	public boolean equals(Song other) {
+    return (name.equals(other.getName()) && artist.equals(other.getArtist()))
+    
+  }
+
+	public String toString() {
+
+	}
+}
